8096eae66f24dfe706f80319231a87e765664a3d
cameronjacoby <cameron.jacoby@gmail.com>
2016-01-23 17:18:10 -0800

    fill in signup

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -77,14 +77,19 @@ app.controller('AuthCtrl', ['$scope', '$auth', '$location',
 
     $scope.signup = function() {
       // signup (https://github.com/sahat/satellizer#authsignupuser-options)
-
-        // set token (https://github.com/sahat/satellizer#authsettokentoken)
-
-        // call $scope.isAuthenticated to set $scope.currentUser
-
-        // clear sign up form
-
-        // redirect to '/profile'
+      $auth.signup($scope.user)
+        .then(function (response) {
+          // set token (https://github.com/sahat/satellizer#authsettokentoken)
+          $auth.setToken(response.data.token);
+          // call $scope.isAuthenticated to set $scope.currentUser
+          $scope.isAuthenticated();
+          // clear sign up form
+          $scope.user = {};
+          // redirect to '/profile'
+          $location.path('/profile');
+        }, function (error) {
+          console.error(error);
+        });
     };
 
     $scope.login = function() {
